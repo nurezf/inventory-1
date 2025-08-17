ef6ef320a4cbb46d2c9838ed9c3393e7653ffd29
nurezf <nuredinzf1996@gmail.com>
2025-08-18 00:24:46 +0300

    set up themes and dark mode toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
+import { ThemeProvider } from "../components/ThemeProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,11 +31,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
+import AppHeader from "./AppHeader/AppHeader";
+import { Card } from "@/components/ui/card";
 
 export default function Home() {
-  return <div className="poppins">this is home page</div>;
+  return (
+    <div className="p-3">
+      <Card className="flex flex-col shadow-none p-2">
+        <AppHeader />
+      </Card>
+    </div>
+  );
 }
