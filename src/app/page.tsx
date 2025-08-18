b1caedf5036f66279120a4ce2d09fe191853b61a
nurezf <nuredinzf1996@gmail.com>
2025-08-18 14:05:50 +0300

    table section header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-import Image from "next/image";
+"use client";
+
 import AppHeader from "./AppHeader/AppHeader";
+import AppTable from "./AppTable/AppTable";
 import { Card } from "@/components/ui/card";
 
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+
 export default function Home() {
+  const { theme } = useTheme();
+  const [isClient, setClient] = useState(false);
+  const bgColor = theme === "dark" ? "bg-black" : "bg-gray-50";
+
+  useEffect(() => {
+    setClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
   return (
-    <div className="p-3">
+    <div className={`poppins p-5  ${bgColor} border w-full min-h-screen`}>
       <Card className="flex flex-col shadow-none p-2">
         <AppHeader />
+        <AppTable />
       </Card>
     </div>
   );
